e2915e0c79a521e0e216e0d5341fb460f4104603
Midnight <midnightytacc@gmail.com>
2026-01-25 19:01:59 +0530

    finished door

--- scenes/houses/door.gd
+++ scenes/houses/door.gd
@@ -6,12 +6,15 @@ extends StaticBody2D
 
 func _ready() -> void:
 	interractable_component.interractable_activated.connect(on_interractable_activated)
-	interractable_component.interractable_activated.connect(on_interractable_deactivated)
+	interractable_component.interractable_deactivated.connect(on_interractable_deactivated)
+	collision_layer = 1
 
 func on_interractable_activated() -> void:
 	animated_sprite_2d.play("open_door")
+	collision_layer = 2
 	print("activated")
 	
 func on_interractable_deactivated() -> void:
 	animated_sprite_2d.play("close_door")
+	collision_layer = 1
 	print("deactivated")
